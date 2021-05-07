ba02c62dfcb15237c5316e3045ef3df219557743
Prue Singh <pruesinghnagra@gmail.com>
2021-05-07 15:44:13 +1200

    added miniCSS & minify dependencies and setup for webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,11 +1,19 @@
 const path = require('path')
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+
 module.exports = {
   entry: ['./client/index.js', './client/styles/main.scss'],
   output: {
     path: path.join(__dirname, '..', 'server', 'public'),
     filename: 'bundle.js'
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.min.css',
+    }),
+  ],
   mode: 'development',
   module: {
     rules: [
@@ -17,12 +25,20 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          'style-loader',
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
           'css-loader',
           'sass-loader'
         ]
       }
     ]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+    ],
   },
   resolve: {
     extensions: ['.js', '.jsx']
